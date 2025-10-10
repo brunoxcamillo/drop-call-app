462df541ac0771093237764a9e8d1e6420e142eb
brunoxcamillo <kamillo.s@hotmail.com>
2025-10-10 08:32:23 -0300

    Delete src directory

--- src/routes/webhooks/zapi.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { handleZapiWebhook } from "../../controllers/zapiWebhookController.js";
-
-const router = Router();
-
-// Agora só JSON normal
-router.post("/", handleZapiWebhook);
-
-export default router;
--- src/utils/normalizePhone.js
+++
@@ -1,6 +0,0 @@
-export function normalizePhone(phone) {
-  if (!phone) return null;
-  // Z-API costuma aceitar dígitos sem "+" em E.164
-  const digits = String(phone).replace(/\D/g, "");
-  return digits.length ? digits : null;
-}
